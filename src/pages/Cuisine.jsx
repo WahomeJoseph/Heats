5ed4598297be2b1f4150440a616275a27e458ed8
wahomeJohn <johnweru206@gmail.com>
2025-02-03 07:16:44 +0300

    API changes

--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -7,15 +7,29 @@ import { Skeleton } from '@mui/material';
 const Cuisine = () => {
     
     const [cuisine, setCuisine] = useState([]);
+    // eslint-disable-next-line no-unused-vars
+    const [error, setError] = useState('')
     const params = useParams();
     console.log(params);
 
     useEffect(() => {
         const getCuisine = async (name) => {
-            const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&cuisine=${name}`);
-            const recipe = await data.json();
-            setCuisine(recipe.results);
-            console.log(cuisine);
+            try {
+            const results = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&cuisine=${name}`);
+            if (!results.ok) {
+                throw new Error('Network Response Failed!');
+            }
+            const recipe = await results.json();
+            if (recipe && recipe.results) {
+                setCuisine(recipe.results);
+                console.log(cuisine);
+            } else {
+                setError('No Recipes Found.');
+            }
+            } catch (err) {
+                setError('Failed to fetch data.');
+                console.error(err, 'No Recipe Data was Fetched!')
+            }
         }
         getCuisine(params.type);
     });
